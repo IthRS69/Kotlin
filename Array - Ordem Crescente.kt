e7ab8fc6c2159976d37918ed9929dcc487a19b12
Ithalo Rodrigues <141924042+IthRS69@users.noreply.github.com>
2023-09-05 09:27:26 -0300

    Add files via upload

---
+++ Array - Ordem Crescente.kt
@@ -0,0 +1,13 @@
+fun main (){
+    val numeros = IntArray(10)
+    println("Digite 10 números inteiros:")
+    for (i in 0 until 10) {
+        println("Número ${i + 1}: ")
+        numeros[i] = readLine()!!.toInt()
+    }
+    numeros.sort()
+    println("Números em ordem Crescente:")
+    for (numero in numeros){
+        println(numero)
+    }
+}
